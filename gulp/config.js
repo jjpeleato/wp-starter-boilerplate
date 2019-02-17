970f22e1050f81f3ba67ff399ddbfbf35111f66e
José J. Peleato Pradel <jose.peleato@quelinka.com>
2019-02-17 22:12:16 +0100

    Implementation gulp v4 tasks for compilation, minification, etc

---
+++ gulp/config.js
@@ -0,0 +1,71 @@
+'use strict';
+
+/* Const for environment */
+const environment = process.env.NODE_ENV || 'development';
+
+/* Activate or desactivate Stylint on CSS task. true: activate | false: desactivate */
+const lintcss = true;
+const jshint = true;
+
+/* Paths */
+const theme = './public/wp-content/themes/ao-apolo/assets/';
+const paths = {
+    sassAssets: {
+        src: [
+            './assets/scss/**/*.scss'
+        ],
+        vendor: [
+            './node_modules/bootstrap/dist/css/bootstrap.min.css'
+        ],
+        dest: theme + 'css/'
+    },
+    jsAssets: {
+        src: [
+            './assets/js/**/*.js'
+        ],
+        vendor: [
+            './node_modules/popper.js/dist/popper.min.js',
+            './node_modules/bootstrap/dist/js/bootstrap.min.js'
+        ],
+        dest: theme + 'js/'
+    },
+    fontAssets: {
+        src: [
+            './assets/fonts/*'
+        ],
+        dest: theme + 'css/fonts/'
+    },
+    imgAssets: {
+        src: [
+            './assets/img/*'
+        ],
+        dest: theme + 'assets/img/'
+    },
+};
+
+/* Options */
+const options = {
+    stylelint: {
+        reporters: [
+            {
+                formatter: 'string',
+                console: true
+            }
+        ]
+    },
+    sass: {
+        outputStyle: 'expanded', // output_style = expanded or nested or compact or compressed
+        precision: 10
+    },
+    uglifyCss: {
+        "maxLineLen": 80,
+        "uglyComments": false
+    }
+};
+
+/* Exports */
+exports.environment = environment;
+exports.lintcss = lintcss;
+exports.jshint = jshint;
+exports.paths = paths;
+exports.options = options;
