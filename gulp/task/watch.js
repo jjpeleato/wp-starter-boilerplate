ff9cbfa14e48d6ffb0f62c93a832e30b4866b485
jjpeleato <jjpeleato@gmail.com>
2019-03-13 20:17:23 +0100

    add svg image and gulp watch validate scss and js

--- gulp/task/watch.js
+++ gulp/task/watch.js
@@ -8,12 +8,14 @@ import gulp from 'gulp';
 import config from '../config';
 import { css } from './styles';
 import { js } from './scripts';
+import { validateScss } from './styles';
+import { validateJs } from './scripts';
 
 /* Tasks */
 function watch()
 {
-    gulp.watch(config.paths.sassAssets.src, css);
-    gulp.watch(config.paths.jsAssets.src, js);
+    gulp.watch(config.paths.sassAssets.src, gulp.series(validateScss, css));
+    gulp.watch(config.paths.jsAssets.src, gulp.series(validateJs, js));
 }
 
 exports.watch = watch;
