27e63940da02863ee64db158575d053a9e2787d0
jjpeleato <jjpeleato@gmail.com>
2019-04-07 21:35:12 +0200

    update yoast seo plugin

--- public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ public/wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit8b3a89cbd6d6b8d784963573d27b4982::getLoader();
+return ComposerAutoloaderInitbb350b1b05a42841542a6db359f3f284::getLoader();
